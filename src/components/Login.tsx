b5e367b818f230a119080f83b90d1cd794a5113b
landtry <landry@boldist.co>
2022-07-07 06:00:37 -0500

    updated callback urls for Auth0

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -22,7 +22,9 @@ export default function Login() {
                   className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-custom-blue hover:bg-custom-blue-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-custom-blue"
                   onClick={(e) => {
                     e.preventDefault();
-                    signIn("auth0", { callbackUrl: "localhost:3000/" });
+                    signIn("auth0", {
+                      callbackUrl: "/",
+                    });
                   }}>
                   Sign in
                 </button>
